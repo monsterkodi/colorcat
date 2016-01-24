e787d075ddc3fe14bd0a8c141b8aaeda8dd91918
monsterkodi <monsterkodi@gmx.net>
2016-01-24 22:39:07 +0100

    readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 ![colorcat](img/colorcat.png)
 ![usage](img/usage.png)
+### examples
+![examples](img/examples.png)
+
+The examples were generated with [this shell script](./test/test.sh).
+
+### syntax highlighting
+
+Contains syntax highligting patterns for
+
+- [coffee](./test/coffee.noon) 
+- [noon](./test/noon.noon) 
+- [json](./test/json.noon) 
+
+A side by side comparison of atom and colorcat highlighting:
+
+![atomcat](img/atomcat.png)
--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env fish
-npm ls --depth 0
-npm ls --depth 0 | colorcat -p "(.──\s)(\S+)(@)([\d\.]+) . xd . g . z . m :: (\S+)(@)([\d\.]+)\s(.*) . yf . z . mf . z"
-npm ls
-npm ls | colorcat -p "([\s┬─├└│]+)(\S+)(@)([\d\.]+) . xd . g . z . m :: (\S+)(@)([\d\.]+)\s(.*) . yf . z . mf . z"
+echo
+echo " Examples " | colorcat -Bfw
+echo
+echo "colorize script messages: " | colorcat -fw
+echo 
+echo ' WARNING! colors ahead!' | colorcat -p "(.+)(!)(.+) . Rfy . Rfr . Yr" 
+echo
+echo "highlight spaces in a file: " | colorcat -fw
+echo
+head -n 4 package.noon | colorcat -p "\s  R :: .* fx" 
+echo
+echo "highlight multiple words: " | colorcat -fw
+echo
+head -n 6 package.noon | colorcat -p "color rf :: cat gf :: git mf :: hub cf :: .* fx" 
+echo
+echo "colorize the output of a command line tool: " | colorcat -fw
+echo
+npm ls --depth 2 | colorcat -p "([\s┬─├└│]+)(\S+)(@)([\d\.]+) . xd . g . z . m :: (\S+)(@)([\d\.]+)\s(.*) . yf . z . mf . z"
+echo "syntax highlighting:" | colorcat -fw
+echo
+cat package.noon | cc -P test/noon.noon 
