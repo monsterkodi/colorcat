095e442c34c8a907b61172d8f369ab8890216ae8
monsterkodi <monsterkodi@gmx.net>
2016-01-30 16:00:56 +0100

    js

--- test/ansi.sh
+++ test/ansi.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 CC="`dirname $0`/../bin/colorcat"
 echo
-echo " Examples " | $CC -aBfw
+echo " Examples " | $CC -saBfw
 echo
-echo "colorize script messages: " | $CC -afw
+echo "colorize script messages: " | $CC -safw
 echo 
-echo ' WARNING! colors ahead!' | $CC -ap "(.+)(!)(.+) . Rfy . Rfr . Yr" 
+echo ' WARNING! colors ahead!' | $CC -sap "(.+)(!)(.+) . Rfy . Rfr . Yr" 
 echo
-echo "highlight spaces in a file: " | $CC -afw
+echo "highlight spaces in a file: " | $CC -safw
 echo
-head -n 4 package.noon | $CC -ap "\s  R :: .* fx" 
+head -n 4 package.noon | $CC -sap "\s  R :: .* fx" 
 echo
-echo "highlight multiple words: " | $CC -afw
+echo "highlight multiple words: " | $CC -safw
 echo
-head -n 6 package.noon | $CC -ap " .* fx :: color r :: cat g :: git m :: hub c" 
+head -n 6 package.noon | $CC -sap " .* fx :: color r :: cat g :: git m :: hub c" 
 echo
-echo "colorize the output of a command line tool: " | $CC -afw
+echo "colorize the output of a command line tool: " | $CC -safw
 echo
-npm ls --depth 2 | $CC -a -p "([\s┬─├└│]+)(\S+)(@)([\d\.]+) . xd . g . zs:- . m :: (\S+)(@)([\d\.]+)\s(.*) . yf . zs:- . mf . z"
-echo "syntax highlighting:" | $CC -afw
+npm ls --depth 2 | $CC -sap "([\s┬─├└│]+)(\S+)(@)([\d\.]+) . xd . g . zs:- . m :: (\S+)(@)([\d\.]+)\s(.*) . yf . zs:- . mf . s:"
 echo
-cat package.noon | $CC -a -P syntax/noon.noon 
+echo "syntax highlighting:" | $CC -safw
+echo
+cat package.noon | $CC -saP syntax/noon.noon 
--- test/test.sh
+++ test/test.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 CC="`dirname $0`/../bin/colorcat"
 echo
-echo " Examples " | $CC -Bfw
+echo " Examples " | $CC -sBfw
 echo
-echo "colorize script messages: " | $CC -fw
+echo "colorize script messages: " | $CC -sfw
 echo 
-echo ' WARNING! colors ahead!' | $CC -p "(.+)(!)(.+) . Rfy . Rfr . Yr" 
+echo ' WARNING! colors ahead!' | $CC -sp "(.+)(!)(.+) . Rfy . Rfr . Yr" 
 echo
-echo "highlight spaces in a file: " | $CC -fw
+echo "highlight spaces in a file: " | $CC -sfw
 echo
-head -n 4 package.noon | $CC -p "\s  R :: .* fx" 
+head -n 4 package.noon | $CC -sp "\s  R :: .* fx" 
 echo
-echo "highlight multiple words: " | $CC -fw
+echo "highlight multiple words: " | $CC -sfw
 echo
-head -n 6 package.noon | $CC -p " .* fx :: color r :: cat g :: git m :: hub c" 
+head -n 6 package.noon | $CC -sp " .* fx :: color r :: cat g :: git m :: hub c" 
 echo
-echo "colorize the output of a command line tool: " | $CC -fw
+echo "colorize the output of a command line tool: " | $CC -sfw
 echo
-npm ls --depth 2 | $CC -p "([\s┬─├└│]+)(\S+)(@)([\d\.]+) . xd . g . zs:- . m :: (\S+)(@)([\d\.]+)\s(.*) . yf . zs:- . mf . z"
-echo "syntax highlighting:" | $CC -fw
+npm ls --depth 2 | $CC -sp "([\s┬─├└│]+)(\S+)(@)([\d\.]+) . xd . g . zs:- . m :: (\S+)(@)([\d\.]+)\s(.*) . yf . zs:- . mf . s:"
 echo
-cat package.noon | $CC - -P syntax/noon.noon 
+echo "syntax highlighting:" | $CC -sfw
+echo
+cat package.noon | $CC -sP syntax/noon.noon 
